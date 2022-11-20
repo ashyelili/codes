880bc9d2ae2e6891e0fd936370336fea3afd8191
ashyelili <112112015+ashyelili@users.noreply.github.com>
2022-11-21 02:18:45 +0800

    Add files via upload
    a sample code of C programming language using dev-c++

---
+++ sample6.cpp
@@ -0,0 +1,19 @@
+/*Write a program that will ask the user to input two strings and displays the
+length of two strings when they are concatenated.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char s1[100], s2[100];
+	
+	printf("Enter the first string\n");
+	gets(s1);
+	printf("Enter the second string\n");
+	gets(s2);
+	strcat(s1,s2);
+	printf("Length of string: %d", strlen(s1));
+	
+	return 0;
+}
